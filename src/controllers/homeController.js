840f83f5caa1ea21074e09b7ad385dace49bfb7e
Gabriel Luiz <gabrielluizcm@hotmail.com>
2022-10-06 19:49:58 -0300

    Doesn't show events if not logged in

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,6 +2,6 @@ const Event = require('../models/EventModel');
 
 exports.index = async (req, res) => {
   const myEvents = req.session.user ? await Event.findMyEvents(req.session.user._id) : [];
-  const events = req.session.user ? await Event.findEvents(req.session.user._id) : await Event.findEvents();
+  const events = req.session.user ? await Event.findEvents(req.session.user._id) : [];
   return res.render('index', { myEvents, events });
 };
